db804631ea3236b74fd313886144ff75e79e6dc2
KozenUmezawa <umezawa@viz.media.kyoto-u.ac.jp>
2016-12-20 21:58:29 +0900

    Add: to test using hierarchical clustering library

--- hierarchical_clustering.py
+++ hierarchical_clustering.py
@@ -1,4 +1,15 @@
+import scipy.spatial.distance
+
 def clustering(allTimeSeries):
-    for timeSeries in allTimeSeries:
-        print(timeSeries)
-    
+    from  scipy.spatial.distance  import pdist
+    from scipy.cluster.hierarchy import linkage, dendrogram
+    from matplotlib.pyplot import show
+
+    pdist = pdist(([[1,1,1], [2,3,4], [5,3,1]]))
+    result = linkage(pdist)
+    dendrogram(result)
+    show()
+    # pDistance = scipy.spatial.distance.pdist(allTimeSeries)
+    # for timeSeries in allTimeSeries:
+    #
+    #     print(timeSeries)
